eafbdb1785f088c40769916123e9a6de7af9caa0
YaronWhite86 <yaronwhite86@gmail.com>
2016-12-19 20:23:11 +0200

    Add files via upload

---
+++ list.h
@@ -0,0 +1,29 @@
+/*
+ * list.h
+ *
+ *  Created on: 22 αιεπ 2016
+ *      Author: Shirel_local
+ */
+
+#ifndef LIST_H_
+#define LIST_H_
+
+typedef struct List_* PList;
+typedef void* PElem;
+typedef enum{FAIL, SUCCESS} Result;
+
+/*User functions*/
+typedef PElem (*CLONE_FUNC)(PElem);
+typedef void (*DESTROY_FUNC)(PElem);
+
+/*Interface functions*/
+PList ListCreate(CLONE_FUNC, DESTROY_FUNC);
+void ListDestroy(PList);
+Result ListAdd(PList, PElem);
+PElem ListGetFirst(PList);
+PElem ListGetNext(PList);
+
+Result ListRemove(PList);
+int ListGetSize(PList);
+
+#endif
